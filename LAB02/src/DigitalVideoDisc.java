03ba539be3d80e7b422a264a5db48117a9107eb3
Nguyen Phan Vi <vi.np2417218@sis.hust.edu.vn>
2026-04-04 09:42:41 +0700

    .

--- LAB02/src/DigitalVideoDisc.java
+++ LAB02/src/DigitalVideoDisc.java
@@ -6,41 +6,11 @@ public class DigitalVideoDisc {
     private String director;
     private int length;
     private float cost;
-    private static int nbDigitalVideoDiscs = 0;
-    private int id;
 
-    public DigitalVideoDisc(String title) {
-        this.title = title;
-        nbDigitalVideoDiscs++;
-        this.id = nbDigitalVideoDiscs;
-    }
-
-    public DigitalVideoDisc(String category, String title, float cost) {
-        this.category = category;
-        this.title = title;
-        this.cost = cost;
-        nbDigitalVideoDiscs++;
-        this.id = nbDigitalVideoDiscs;
-    }
-
-    public DigitalVideoDisc(String director, String category, String title, float cost) {
-        this.director = director;
-        this.category = category;
-        this.title = title;
-        this.cost = cost;
-        nbDigitalVideoDiscs++;
-        this.id = nbDigitalVideoDiscs;
-    }
-
-    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
-        this.title = title;
-        this.category = category;
-        this.director = director;
-        this.length = length;
-        this.cost = cost;
-        nbDigitalVideoDiscs++;
-        this.id = nbDigitalVideoDiscs;
-    }
+    public DigitalVideoDisc(String title) { super(); this.title = title; }
+    public DigitalVideoDisc(String category, String title, float cost) { super(); this.category = category; this.title = title; this.cost = cost; }
+    public DigitalVideoDisc(String director, String category, String title, float cost) { super(); this.director = director; this.category = category; this.title = title; this.cost = cost; }
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) { super(); this.title = title; this.category = category; this.director = director; this.length = length; this.cost = cost; }
 
     public String getTitle() { return title; }
     public String getCategory() { return category; }
